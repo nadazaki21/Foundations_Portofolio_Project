9553917632f8dd603e82a5bb96860d0e86fc73c7
nadazkai21 <nadazaki019@gmail.com>
2024-06-17 00:04:46 +0300

    dashboard done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DashBoard from './components/DashBoard/DashBoard';
+
+
+function App() {
+  return (
+  
+    <div className='app'>
+        <DashBoard/>
+    </div>
+    
+  );
+}
+
+export default App;
