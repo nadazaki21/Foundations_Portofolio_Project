778d0e61054a31d82cddc89f4a142f1fd9ca6ae3
nadazkai21 <nadazaki019@gmail.com>
2024-06-18 00:51:42 +0300

    login and sign up added seprately

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import DashBoard from './components/DashBoard/DashBoard';
+import Login from './components/LoginSignup/login';
+import Signup from './components/LoginSignup/signup';
 
 
 function App() {
   return (
   
     <div className='app'>
-        <DashBoard/>
+        <Signup/>
     </div>
     
   );
